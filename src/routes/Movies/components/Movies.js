cb5a3b7b22acf7ab5f5180b34d7f0b300e1a4916
Matthew Ma <justmxk@gmail.com>
2017-10-24 23:19:25 +1100

    get entries and filter movies

--- src/routes/Movies/components/Movies.js
+++ src/routes/Movies/components/Movies.js
@@ -16,11 +16,66 @@ class Movies extends Component {
     super(props)
   }
 
-  render () {
+  componentDidMount() {
+    this.props.getEntries()
+  }
 
+  componentWillUnmount() {
+    this.props.clearEntries()
+  }
+
+  renderLoading = () => {
+    return (
+      <Row>
+        <span className='message'>isLoading...</span>
+      </Row>
+    )
+  }
+
+  renderError = () => {
+    return (
+      <Row>
+        <span className='message'>Opps. Something went wrong...</span>
+      </Row>
+    )
+  }
+
+  filterMovies = (entries) => {
+    const filtered_movies = filter(entries, (m) => {
+      return m["releaseYear"] >= 2010 && m['programType'] === 'movie'
+    }).slice(0, 21)
+    return orderBy(filtered_movies, ['title'],['asc'])
+  }
+
+  renderMovies = (entries) => {
+    const display_movies = this.filterMovies(entries)
+    return (
+      <div id='entity_view'>
+        {display_movies.map((movie, index) => {
+          return <EntityComponent title={movie.title} url={movie.images['Poster Art']['url']} key={index}/>
+        })}
+      </div>
+    )
+  }
+
+  render () {
+    const {entries, error, isLoading} = this.props.entries
+    let renderContent
+    if (isLoading) {
+      renderContent = this.renderLoading()
+    } else if(error) {
+      renderContent = this.renderError()
+    } else if (entries.length > 0) {
+      renderContent = this.renderMovies(entries)
+    }
     return (
       <div className='' id='movie'>
-        Movie View
+        <TitleBar title='Popular Movies'/>
+          <section>
+            <Grid>
+              {renderContent}
+            </Grid>
+          </section>
       </div>
     )
   }
